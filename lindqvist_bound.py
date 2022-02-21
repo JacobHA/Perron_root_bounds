d09f840c84b2543b6e75982fceecdbedbf746249
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2022-02-20 19:32:09 -0500

    dependency on linalg

--- lindqvist_bound.py
+++ lindqvist_bound.py
@@ -1,25 +1,6 @@
 import numpy as np
+from linalg_functions import Perron_info
 
-
-def DOMeigs(M):
-    # returns dominant eigen-information for matrix M
-    vals,vecs = np.linalg.eig(M) # Eigen
-    vecs = (vecs.T).real # format and realize
-    dict_of_eig = dict(zip(vals,vecs))
-    dom_eig_val=sorted(dict_of_eig)[-1]
-    dom_eig_vec=dict_of_eig[dom_eig_val]
-    
-    assert dom_eig_val == dom_eig_val.real, ("Complex Perron root!")
-    return dom_eig_val.real, dom_eig_vec
-
-def Perron_info(M):
-    # returns rho, u, v
-    rho,v = DOMeigs(M)
-    rho,u = DOMeigs(M.T)
-    v /= sum(v)
-    u /= u.dot(v)
-    return rho, u, v
-  
 def gamma_inf(A,B):
     N = A.shape[0]
     rA,x,y = Perron_info(A)
