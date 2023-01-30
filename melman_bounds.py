eacd0ac35a648044f4d6bb99e160dbf78f0bc8bb
JacobHA <jacobhadamczyk@gmail.com>
2023-01-29 19:46:25 -0500

    cleanup melman

--- melman_bounds.py
+++ melman_bounds.py
@@ -5,6 +5,13 @@ Created on Tue Feb 15 13:18:34 2022
 @author: jacob
 """
 
+# Import required packages
+from numpy import ndarray, any, identity, diag, inf, fill_diagonal, sqrt, zeros_like
+
+from numpy import random
+from numpy.linalg import eigvals
+import time
+
 
 def Melman_bounds(A):
     """
@@ -22,11 +29,8 @@ def Melman_bounds(A):
     -------
     numpy.ndarray
         The upper and lower bounds for A's Perron root.
-        
-    """
 
-    # Import required packages
-    from numpy import ndarray, any, identity, diag, inf, fill_diagonal, sqrt, zeros_like
+    """
 
     # Check that M is a 2D numpy array
     if not isinstance(A, ndarray):
@@ -40,24 +44,24 @@ def Melman_bounds(A):
 
     # Check that M is non-negative
     if any(A < 0):
-        raise ValueError("Input A must be non-negative.")        
-
+        raise ValueError("Input A must be non-negative.")
 
     # R_i(A) in Melman's notation:
-    R = A.sum(axis=1) # Calculate the row sums of M
+    R = A.sum(axis=1)  # Calculate the row sums of M
 
     # R'_i(A) in Melman's notation:
-    Rp = R - diag(A) 
+    Rp = R - diag(A)
 
     # R''_i(A) in Melman's notation:
     Rpp = Rp - A
 
     # Calculate the bounds on the Perron root, based on eq. (5) and (6) in Melman's paper
     bounds = zeros_like(A)
-    for i,A_ii in enumerate(diag(A)):
-        for j,A_jj in enumerate(diag(A)):
+    for i, A_ii in enumerate(diag(A)):
+        for j, A_jj in enumerate(diag(A)):
             if i != j:
-                bounds[i][j] = A_ii + A_jj + Rpp[i][j] + sqrt( (A_ii - A_jj + Rpp[i][j])**2 + 4*A[i][j]*Rp[j])
+                bounds[i][j] = A_ii + A_jj + Rpp[i][j] + \
+                    sqrt((A_ii - A_jj + Rpp[i][j])**2 + 4*A[i][j]*Rp[j])
 
     # Mask the diagonal elements (i!=j)
     fill_diagonal(bounds, -inf)
@@ -69,11 +73,9 @@ def Melman_bounds(A):
 
 def main():
     # Test the bounds
-    from numpy import random
-    from numpy.linalg import eigvals
-    import time
+
     # Generate a random matrix, 1_000x1_000 with range 0-1 (uniform distribution)
-    A = random.rand(1_000, 1_000) 
+    A = random.rand(1_000, 1_000)
     start = time.process_time()
     LB, UB = Melman_bounds(A)
     print(f"Time taken for bound calculation: {time.process_time() - start}s")
